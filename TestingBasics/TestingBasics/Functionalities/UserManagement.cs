8b00ad96c2f2786bc04770f54518fec4799e8376
khatanboyevelbek <programmerelbek@gmail.com>
2022-11-29 10:36:06 +0500

    Remove Some Comments

--- TestingBasics/TestingBasics/Functionalities/UserManagement.cs
+++ TestingBasics/TestingBasics/Functionalities/UserManagement.cs
@@ -1,38 +1,5 @@
 ﻿namespace TestingBasics.Functionalities
 {
-    //public record User(string FirstName, string LastName)
-    //{
-    //    public int Id { get; init; }
-    //    public DateTime CreatedDate { get; init; } = DateTime.UtcNow;
-    //    public string Phone { get; set; } = "+44 ";
-    //    public bool VerifiedEmail { get; set; } = false;
-    //}
-
-    //public class UserManagement
-    //{
-    //    private readonly List<User> users = new();
-    //    private int idCounter = 1;
-
-    //    public IEnumerable<User> AllUsers => users; 
-
-    //    public void Add(User user)
-    //    {
-    //        users.Add(user with { Id = idCounter++ });
-    //    }
-
-    //    public void UpdatePhone(User user)
-    //    {
-    //        User dbUser = users.First(x => x.Id == user.Id);
-    //        dbUser.Phone = user.Phone;
-    //    }
-
-    //    public void VerifyEmail(int id)
-    //    {
-    //        User dbUser = users.First(x => x.Id == id);
-    //        dbUser.VerifiedEmail = true;
-    //    }
-    //}
-
     public record Product(int Id, string Name, double price);
 
     public interface IDbService
